ad88ed281db3ac629faf4a6d0d517ec4350ac513
hanuri5155 <hanuri5155@gmail.com>
2025-06-24 18:42:51 +0900

    Websocket 응답 추가

---
+++ src/main/java/com/orchid/springbackend/controller/WebSocketController.java
@@ -0,0 +1,14 @@
+package com.orchid.springbackend.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import com.orchid.springbackend.dto.SensorDataDto;
+
+public class WebSocketController {
+    @MessageMapping("/hello") // 클라이언트가 /app/hello 로 메시지 보낼 때
+    @SendTo("/topic/sensor")  // 모든 구독자에게 브로드캐스트
+    public SensorDataDto sendSensor(SensorDataDto dto) {
+        System.out.println("WebSocket 수신됨: " + dto);
+        return dto; // 그대로 브로드캐스트
+    }
+}
